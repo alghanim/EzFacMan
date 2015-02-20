afef99e3277911282307177640977fb39035c08e
tud13865 <tud13865@CIS-SE35918.tu.temple.edu>
2015-02-20 09:50:27 -0500

    Added Help Menu buttons, added quit functionality, renamed some variables

--- src/EZFacUI.java
+++ src/EZFacUI.java
@@ -67,7 +67,7 @@ public class EZFacUI extends javax.swing.JFrame {
         floorLabel = new javax.swing.JLabel();
         campusLabel = new javax.swing.JLabel();
         buildingLabel = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
+        spreadsheetPanel = new javax.swing.JScrollPane();
         spreadsheetTable = new javax.swing.JTable();
         occupancy = new javax.swing.JLabel();
         jComboBox4 = new javax.swing.JComboBox();
@@ -362,7 +362,7 @@ public class EZFacUI extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(spreadsheetTable);
+        spreadsheetPanel.setViewportView(spreadsheetTable);
 
         occupancy.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         occupancy.setText("Ocupancy");
@@ -383,7 +383,7 @@ public class EZFacUI extends javax.swing.JFrame {
                 .addGap(184, 184, 184)
                 .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1127, Short.MAX_VALUE)
+                        .addComponent(spreadsheetPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 1127, Short.MAX_VALUE)
                         .addGap(52, 52, 52))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
                         .addGap(326, 326, 326)
@@ -420,7 +420,7 @@ public class EZFacUI extends javax.swing.JFrame {
                         .addComponent(floorLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(floorDropdown, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 371, Short.MAX_VALUE)
+                .addComponent(spreadsheetPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 371, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -618,7 +618,6 @@ public class EZFacUI extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JFrame loginScreen;
     private javax.swing.JPanel mainPanel;
@@ -628,6 +627,7 @@ public class EZFacUI extends javax.swing.JFrame {
     private javax.swing.JMenuItem quitButton;
     private javax.swing.JMenuItem resetDeptColors;
     private javax.swing.JMenu settingsButton;
+    private javax.swing.JScrollPane spreadsheetPanel;
     private javax.swing.JTable spreadsheetTable;
     private javax.swing.JMenuItem userManualButton;
     // End of variables declaration//GEN-END:variables
