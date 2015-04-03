43a318941d12a19ce7bb2016fbdc50c621a3f26e
Ali <Ali@2c-f0-ee-1c-b6-20.wireless.temple.edu>
2015-04-03 11:10:45 -0400

    - changed mapDisplay size - changed jLabel to jTextfields

--- src/databaseTables/RoomsManager.java
+++ src/databaseTables/RoomsManager.java
@@ -4,10 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Scanner;
-import EzFacMan.EZFacUI;
 
 /**
  * Manages SQL statements to retrieve the data from the database
